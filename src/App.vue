c24a7516ea8259deedf620e8e9e040708077f56e
Connor Frendt <cjacksonfx@yahoo.com>
2018-11-27 16:12:44 -0800

    wired up app.vue

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <!-- <Header
+      v-bind:sort="sort"
+      v-bind:filter="filter"
+      v-bind:types="pokemonTypes"/> -->
+
+    <Pokedex v-bind:pokedex="pokedex"/>
+  </div>
+</template>
+
+<script>
+import pokedexApi from './services/pokedex-api.js';
+import Pokedex from './components/Pokedex.vue';
+
+export default {
+  data() {
+    return {
+      pokedex: pokedexApi.getPokedex(),
+    }
+  },
+  components: {
+    Pokedex
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/services/pokedex-api.js
@@ -0,0 +1,9 @@
+import pokedex from './pokedex.js';
+
+const pokedexApi = {
+    getPokedex() {
+        return pokedex;
+    }
+};
+
+export default pokedexApi;
